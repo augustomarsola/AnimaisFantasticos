60dd50eb367e21231455211fe83b690e1194263c
Augusto Marsola <augusto.marsola@live.com>
2022-03-01 21:13:11 -0300

    Finalizada refatoração no Accordion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAccodrion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import iniTabNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -13,7 +13,9 @@ import initAnimaScroll from "./modules/scroll-animacao.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAccodrion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 iniTabNav();
 initModal();
 initTooltip();
