7df7915488f0ddc5f86d444c02556a2821cecbd4
Augusto Marsola <augusto.marsola@live.com>
2022-03-09 23:19:13 -0300

    Horário de funcionamento Refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,15 +1,44 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+export default class Funcionamento {
+  constructor(semana, active) {
+    this.funcionamento = document.querySelector(semana);
+    this.activeClass = active;
+  }
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horaAgora = dataAgora.getHours();
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(",")
+      .map(Number);
+  }
 
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto = (horaAgora >= horarioSemana[0] && horaAgora < horarioSemana[1]);
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horaAgora = this.dataAgora.getUTCHours() - 3;
+  }
 
-  if (semanaAberto && horarioAberto)
-    funcionamento.classList.add('aberto');
+  estaAberto() {
+    this.semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    this.horarioAberto =
+      this.horaAgora >= this.horarioSemana[0] &&
+      this.horaAgora < this.horarioSemana[1];
+
+    return this.semanaAberto && this.horarioAberto;
+  }
+
+  addFuncionamentoEvent() {
+    if (this.estaAberto) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.addFuncionamentoEvent();
+    }
+
+    return this;
+  }
 }
